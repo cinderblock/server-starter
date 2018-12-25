863ba32ccc4ca81a0b7fc91ee3eb33b52b8ca7c8
Cameron Tacklind <cameron@tacklind.com>
2018-12-25 14:47:58 -0800

    Add declarations file for TS

---
+++ serverStarter.d.ts
@@ -0,0 +1,22 @@
+export default function serverStarter(
+  server: {
+    listen:
+      | ((listen: number, bind?: string) => any)
+      | ((listen: string) => any);
+    once: (event: 'error' | 'listening', cb: (...args: any[]) => any) => any;
+  },
+  config:
+    | {
+        listen: string;
+        socketMode: number;
+        socketOwner: {
+          user: number | string;
+          group: number | string;
+        };
+      }
+    | {
+        listen: number;
+        hostname?: string;
+      },
+  callback: (err: bool, info: string, extra?: string) => void
+): void;
