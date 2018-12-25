c3c4976d48fc70a22034b4dbe4577c3251799987
Cameron Tacklind <cameron@tacklind.com>
2018-12-25 14:46:00 -0800

    Fix typo

--- serverStarter.js
+++ serverStarter.js
@@ -4,7 +4,9 @@ const net = require('net');
 function serverStarter (server, config, callback) {
 
   if (typeof(server.listen) != 'function') {
-    throw new TypeError('server ardument must have a member function called `listen`');
+    throw new TypeError(
+      'server argument must have a member function called `listen`'
+    );
   }
 
   if (!callback) callback = function(err, data) {
