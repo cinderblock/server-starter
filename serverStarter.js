5051265ebf3ad51bc079349468721e3702365929
Cameron Tacklind <cameron@tacklind.com>
2018-12-25 14:47:18 -0800

    Prettier

--- serverStarter.js
+++ serverStarter.js
@@ -1,19 +1,19 @@
 const fs = require('fs');
 const net = require('net');
 
-function serverStarter (server, config, callback) {
-
-  if (typeof(server.listen) != 'function') {
+function serverStarter(server, config, callback) {
+  if (typeof server.listen != 'function') {
     throw new TypeError(
       'server argument must have a member function called `listen`'
     );
   }
 
-  if (!callback) callback = function(err, data) {
-    if (err) {
-      throw {err, data};
-    }
-  };
+  if (!callback)
+    callback = function(err, data) {
+      if (err) {
+        throw { err, data };
+      }
+    };
 
   function StartListening() {
     server.listen(config.listen, config.hostname);
@@ -25,7 +25,7 @@ function serverStarter (server, config, callback) {
     function done() {
       callback(null, addr);
     }
-    if (addr.port && (addr.port > 0)) {
+    if (addr.port && addr.port > 0) {
       // Listening on a port. We're done here.
       done();
       return;
@@ -45,21 +45,27 @@ function serverStarter (server, config, callback) {
       if (isPosInt(config.socketOwner.user)) {
         user = config.socketOwner.user;
         set = true;
-      } else if (typeof config.socketOwner.user === 'string' && (req = requireUserId())) {
+      } else if (
+        typeof config.socketOwner.user === 'string' &&
+        (req = requireUserId())
+      ) {
         user = req.uid(config.socketOwner.user);
         set = true;
       }
       if (isPosInt(config.socketOwner.group)) {
         group = config.socketOwner.group;
         set = true;
-      } else if (typeof config.socketOwner.group === 'string' && (req = requireUserId())) {
+      } else if (
+        typeof config.socketOwner.group === 'string' &&
+        (req = requireUserId())
+      ) {
         group = req.gid(config.socketOwner.group);
         set = true;
       }
       // Only if we actually set a user or group properly does this do anything
       if (set) {
         // Redefine done2, to be called shortly.
-        done2 = function () {
+        done2 = function() {
           fs.chown(addr, user, group, done);
         };
       }
@@ -86,20 +92,23 @@ function serverStarter (server, config, callback) {
         // Try to connect. If not running, remove and continue.
         var clientSocket = new net.Socket();
 
-        clientSocket.on('error', function (e) {
+        clientSocket.on('error', function(e) {
           if (e.code == 'ECONNREFUSED') {
             // Remove and continue
             fs.unlink(err.address, StartListening);
           } else {
-            done('Unkown error', e);
+            done('Unknown error', e);
           }
         });
 
         // Try to connect
-        clientSocket.connect({path: err.address}, function() {
-          done(`Address (${err.address}) already in use`);
-          clientSocket.unref();
-        });
+        clientSocket.connect(
+          { path: err.address },
+          function() {
+            done(`Address (${err.address}) already in use`);
+            clientSocket.unref();
+          }
+        );
       } else {
         // Unknown address in use error
         done('Address in use');
@@ -114,16 +123,16 @@ function serverStarter (server, config, callback) {
   server.once('error', handleError);
 
   StartListening();
-};
+}
 
 function isPosInt(i) {
-  return (typeof i === 'number') && (i % 1 === 0) && (i >= 0);
+  return typeof i === 'number' && i % 1 === 0 && i >= 0;
 }
 
 var userid;
 
 function requireUserId() {
-  return userid = userid || require('userid');
+  return (userid = userid || require('userid'));
 }
 
 module.exports = serverStarter;
